0b8f156b0fda89d85bfb64b402aa168dc6149853
Roger Viñas <roger.vinas@n26.com>
2022-10-20 15:36:52 +0200

    Use random port

--- sample-api-server/src/test/kotlin/com/rogervinas/sample/api/server/SampleApiServerContractTest.kt
+++ sample-api-server/src/test/kotlin/com/rogervinas/sample/api/server/SampleApiServerContractTest.kt
@@ -1,29 +1,39 @@
 package com.rogervinas.sample.api.server
 
+import au.com.dius.pact.provider.junit5.HttpTestTarget
 import au.com.dius.pact.provider.junit5.PactVerificationContext
 import au.com.dius.pact.provider.junitsupport.Provider
 import au.com.dius.pact.provider.junitsupport.State
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestTemplate
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.server.LocalServerPort
 import java.time.LocalDate
 
 
-@ExtendWith(SpringExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @Provider("Sample API Server")
 @PactBroker
+@ExtendWith(PactVerificationSpringProvider::class)
 class SampleApiServerContractTest {
 
   @Autowired
   private lateinit var repository: SampleRepository
 
+  @LocalServerPort
+  private var port = 0
+
+  @BeforeEach
+  fun beforeEach(context: PactVerificationContext) {
+    context.target = HttpTestTarget("localhost", port)
+  }
+
   @TestTemplate
-  @ExtendWith(PactVerificationSpringProvider::class)
   fun pactVerificationTestTemplate(context: PactVerificationContext) {
     context.verifyInteraction()
   }
@@ -35,7 +45,7 @@ class SampleApiServerContractTest {
 
   @State("Thing 123 exists")
   fun `thing 123 exists`() {
-    repository.reset(125)
+    repository.reset(123)
     repository.save(SampleThing("Foo", 123.45, LocalDate.of(2022, 10, 13)))
   }
 }
