ad9ace716a5742fe260aed1e27ff0147f54af360
Roger Viñas Alcon <roger.vinas@fastzink.com>
2025-12-07 18:12:51 +0100

    Add deps

--- sample-api-server/build.gradle.kts
+++ sample-api-server/build.gradle.kts
@@ -36,13 +36,12 @@ kotlin {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("io.projectreactor:reactor-test")
+  testImplementation("io.mockk:mockk:1.14.6")
 
   testImplementation("au.com.dius.pact.provider:spring6:4.6.18")
 }
